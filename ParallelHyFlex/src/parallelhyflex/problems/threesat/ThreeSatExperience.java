b715c902017a56ff4569193a67b72d2aeb4c4627
KommuSoft <vanonsem.willem@gmail.com>
2013-03-21 16:00:19 +0100

    introduction of 3SAT-EXP

---
+++ ParallelHyFlex/src/parallelhyflex/problems/threesat/ThreeSatExperience.java
@@ -0,0 +1,20 @@
+package parallelhyflex.problems.threesat;
+
+import parallelhyflex.problemdependent.ExperienceBase;
+
+/**
+ *
+ * @author kommusoft
+ */
+public class ThreeSatExperience extends ExperienceBase<ThreeSatSolution,ThreeSatProblem> {
+    
+    public ThreeSatExperience (ThreeSatProblem problem) {
+        super(problem);
+    }
+
+    @Override
+    public void join(ThreeSatSolution solution) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
