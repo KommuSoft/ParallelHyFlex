baa48bae1a5260251cfce261342aa3f8a71e5332
KommuSoft <vanonsem.willem@gmail.com>
2013-05-20 09:14:37 +0200

    masterthesis

--- clean.sh
+++ clean.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for p in {"*.~","*.*~*","*.backup","*.aux","*.bbl","*.blg","*.ilg","*.ind","*.lof","*.log","*.lot","*.out","*.toc","*.loa"}
+for p in {"*.~","*.*~*","*.backup","*.aux","*.bbl","*.blg","*.ilg","*.ind","*.lof","*.log","*.lot","*.out","*.toc","*.loa","*.bak"}
 do
 	fall=$(find . -name "$p")
 	for f in $fall
